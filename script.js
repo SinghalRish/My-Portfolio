45cd76d7094e6e1f26199922e67325a2fc96899a
SinghalRish <singhalrishabh18@gmail.com>
2025-08-13 11:31:52 +0530

    chat bot added can be used later

--- script.js
+++ script.js
@@ -33,3 +33,38 @@ let letter = "";
     setTimeout(type, 80);
   }
 })();
+
+
+const botReplies = {
+  "hi": "Hello! I'm RishBot, here to tell you about Rishabh.",
+  "who are you": "I'm Rishabh's portfolio assistant!",
+  "projects": "I have worked on TastyBurger Delivery, Tomato App, and more!",
+  "skills": "HTML, CSS, JavaScript, React, Angular, Node.js, and more!",
+  "contact": "You can reach Rishabh at rishabhsinghal061@gmail.com"
+};
+
+
+// chat bot functionality can be added later make changes accorddingly 
+// function sendMessage() {
+//   const input = document.getElementById("user-input");
+//   const message = input.value.toLowerCase();
+  
+//   addMessage("You", message, "user");
+  
+//   let reply = "Sorry, I don't understand. Try asking about 'projects', 'skills', or 'contact'.";
+//   for (let key in botReplies) {
+//     if (message.includes(key)) {
+//       reply = botReplies[key];
+//       break;
+//     }
+//   }
+  
+//   addMessage("Bot", reply, "bot");
+//   input.value = "";
+// }
+
+// function addMessage(sender, text, className) {
+//   const messages = document.getElementById("messages");
+//   messages.innerHTML += `<div class="${className}"><strong>${sender}:</strong> ${text}</div>`;
+//   messages.scrollTop = messages.scrollHeight;
+// }
